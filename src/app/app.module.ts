6bb301ee68e4d6f08e741c8d50d6ac878cd82493
alexspom <alexjos00@gmail.com>
2021-04-26 21:13:17 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModulo } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModulo
   ],
   providers: [],
   bootstrap: [AppComponent]
